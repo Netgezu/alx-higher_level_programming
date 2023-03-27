1344eec393b703c2dcc6bdb4d6f027d0d8bf0888
Netgezu <106767573+Netgezu@users.noreply.github.com>
2023-03-27 16:29:43 +0300

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
-// function that returns the number of occurrences in a list
-
-const originalList = require('./100-data').list;
-console.log(originalList);
-const mappedList = originalList.map (function (e, index) {
-  return (e * index);
-});
-console.log(mappedList);
+/* Task 101 - script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence.  import dict from the file 101-data.js */
+const dict = require('./101-data').dict;
+const sort = (obj) => {
+  const reverse = {};
+  /* A key is a number of occurrences - A value is the list of user ids */
+  Object.keys(obj).forEach((key) => {
+    reverse[obj[key]] = reverse[obj[key]] || [];
+    reverse[obj[key]].push(key);
+  });
+  return reverse;
+};
+console.log(sort(dict));
