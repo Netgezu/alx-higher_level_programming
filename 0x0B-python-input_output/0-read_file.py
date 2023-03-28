39c322fd29433365e36dafed7d4b9d3878f9385a
Netgezu <106767573+Netgezu@users.noreply.github.com>
2023-03-28 11:19:21 +0300

    Delete 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Mdoule Task 0"""
-
-
-def read_file(filename=""):
-    """To print the content of a text file."""
-    with open(filename, 'r') as f:
-        print(f.read(), end='')
