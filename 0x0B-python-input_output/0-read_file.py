03e68b8626e24a75ddeb4cf53d54125f51364ca6
Netgezu <106767573+Netgezu@users.noreply.github.com>
2023-03-28 11:08:11 +0300

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-""" Function that reads a text file (UTF8) and prints it to stdout: """
+"""Mdoule Task 0"""
 
 
 def read_file(filename=""):
-    """ read_file method:
-        Args:
-            filename = name of file(string)
-        Returns:
-            None
-    """
-    with open(filename, encoding='utf-8') as f:
-        for x in f:
-            print(x, end="")
+    """To print the content of a text file."""
+    with open(filename, 'r') as f:
+        print(f.read(), end='')
