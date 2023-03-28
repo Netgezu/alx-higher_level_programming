4821194407f8ef292775ad021a5cad5a95263d3f
Netgezu <106767573+Netgezu@users.noreply.github.com>
2023-03-28 11:04:16 +0300

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-"""read_file module.
-
-Contains a function that reads a text file.
+"""Module 0-read_file.
+Reads from a file and prints.
 """
 
 
 def read_file(filename=""):
-    """Reads a text file (UTF8) and prints it to stdout."""
-    with open(filename, 'r') as f:
-        print(f.read(), end='')
+    """Reads from filename and prints
+    its contents to stdout.
+
+    Args:
+        - filename: name of the file
+    """
+
+    with open(filename) as f:
+        read_text = f.read()
+        print(read_text, end="")
